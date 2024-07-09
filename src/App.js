35012e461fd222de3007c17a71babb3c1e04fea0
madelvilla <villa.lourdes11@gmail.com>
2024-07-08 21:12:40 -0400

    very easy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello, my name is Lourdes Villa. My phone number is 123-123-5678. I was born May 4, 1998. 
+      </div>
+    )
+  }
 }
-
-export default App;
